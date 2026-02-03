dacae0dac4c4adcfbc736b9c3ff29ced9ab4a0d6
Rinoa Janelle Jamil <rinoajanellejamil@gmail.com>
2026-02-03 22:11:51 +0800

    Restaurant 1.1

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,2 +1,10 @@
 public class Restaurant {
+    public static void main(String[] args) {
+
+        Chef chef1 = new Chef();
+        Chef chef2 = new Chef();
+
+        Kitchen kitchen1 = new Kitchen();
+        Kitchen kitchen2 = new Kitchen();
+    }
 }
