4d60b15fc42a27ce4779cd4ce1c95c34b92cbb58
Angel Mae Mejias <angelmaemejias11@gmail.com>
2026-02-03 22:26:00 +0800

    Restaurant 1.2

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -6,5 +6,17 @@ public class Restaurant {
 
         Kitchen kitchen1 = new Kitchen();
         Kitchen kitchen2 = new Kitchen();
+
+        System.out.println(
+                // Identity methods
+                chef1.show_identity() + "\n" +
+                        chef2.show_identity() + "\n" +
+                        kitchen1.showIdentity() + "\n" +
+                        kitchen2.showIdentity() + "\n\n" +
+
+                        // Functional output
+                        kitchen1.generateKitchenStatus() + "\n" +
+                        kitchen2.generateKitchenStatus()
+        );
     }
 }
